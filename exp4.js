346e7ffd686cbb3e1d52725152eb497b075ab410
Isha Patil <ishapatil120306@gmail.com>
2026-03-05 21:04:37 +0530

    Frontend

---
+++ exp4.js
@@ -0,0 +1,9 @@
+var no= prompt("Enter a number:");
+if(n%2==0){
+    document.write("The number is even.");
+}
+else{
+    document.write("The number is odd.");
+}
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+var no= prompt("Enter a number:");
+if(n%2==0){
+    document.write("The number is even.");
+}
+else{
+    document.write("The number is odd.");
+}
